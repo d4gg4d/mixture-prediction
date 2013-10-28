a72a4ea2e61374155653dcd4c44cfbc09b752f0e
Sami Airaksinen <samiaira@gmail.com>
2013-10-28 21:49:27 +0200

    added first partition functions and their simple test.

--- pkg/R/sampling.R
+++ pkg/R/sampling.R
@@ -1,5 +1,13 @@
 
 getSampleOf <- function(data, size=10, portion=NULL) {
-  sampleSize <- ifelse(portion == NULL, size, portion*nrow(data))
-  return(data[sample(1:nrow(data),as.integer(sampleSize)),]);
+  sampleSize <- ifelse(is.null(portion), size, portion*nrow(data))
+  return(data[sample(1:nrow(data), as.integer(sampleSize)),]);
 };
+
+filterUser <- function(id, data) {
+  return(data[data$userid == id,]);
+}
+
+getBetween <- function(data, start, end) {
+  return(data[data$time >= as.numeric(start) & time <= as.numeric(end),]);
+}
