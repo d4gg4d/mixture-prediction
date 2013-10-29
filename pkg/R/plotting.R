4ec06065d2e6ed1f1eaa7f18337c19840f520639
Sami Airaksinen <samiaira@gmail.com>
2013-10-29 22:17:50 +0200

    fixing eval error with qmap options

--- pkg/R/plotting.R
+++ pkg/R/plotting.R
@@ -3,14 +3,14 @@ plotGisDistribution <- function(gisData) {
   locationStatistics <- stat_sum(aes(x=longitude, y=latitude, colour=type, group=type), data=gisData, alpha=.6);
 
   ## map around median position
-  mapCenter <- median( c(gisData$longitude, gisData$latitude), na.rm=TRUE);
-  zoomLevel <- 11;
-  map <- qmap(mapCenter, zoom=zoomLevel);
+  mapCenter <- sapply( list(gisData$longitude, gisData$latitude), FUN=function(x) {return(median(x, na.rm=TRUE))});
+  zoomLevel <- 11; ##todo figure out Error in eval(expr, envir, enclos) (from #8) : object 'zoomLevel' not found
+  map <- qmap(mapCenter, zoom=11);
   medianPlace <- map + locationStatistics + scale_size(range = c(2, 20)) + ggtitle(paste("Around median position, zoom", zoomLevel));
 
   ## around lausanne
   zoomLevel <- 9;
-  map <- qmap("lausanne", zoom=zoomLevel);
+  map <- qmap("lausanne", zoom=9);
   overallPlace <- map + locationStatistics + scale_size(range = c(1, 10)) + ggtitle(paste("Around Lausanne, zoom", zoomLevel));
 
   ## overall time distibution
