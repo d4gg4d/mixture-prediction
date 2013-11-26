960c73e52c8bf3c11d98b3499d276e5dd79a9efe
Sami Airaksinen <samiaira@gmail.com>
2013-11-26 23:15:51 +0200

    debugged trough the mixture pipe.
    - first meaningful values from input.

--- pkg/R/mixture-predictor.R
+++ pkg/R/mixture-predictor.R
@@ -1,5 +1,6 @@
 
 ## takes in list of models, data.frame of new data, function of mixture predictor.
+library(plyr)
 
 ## returns vector/matrix of mixture response corresponding to size of new data.
 mixture.predict <- function(models, newdata, score.fn, mixture) {
@@ -7,19 +8,22 @@ mixture.predict <- function(models, newdata, score.fn, mixture) {
   history.models <- list(trained.models=models);
 
   # returns prediction vectors in data.frame
-  prediction.prototype.call <- function(hmodel, target.data) {
-    ##todo mangle data for each prediction variable inner-model
-    return(data.frame(predict(hmodel$trained.model, target.data, interval="confidence")));
+  prediction.prototype.call <- function(model.pair, target.data) {
+    latitude <- data.frame(predict(model.pair$latitude, target.data, interval="confidence"));
+    longitude <- data.frame(predict(model.pair$longitude, target.data, interval="confidence"));
+    return(cbind(latitude=latitude, longitude=longitude));
   }
 
   # returns score vector length of prediction row count 
-  referee.prototype.call <- function(hmodel, target.data, score.function) {
-    predicions <- hmodel$predictions$fit;
-    return(score.function(predictions, target.data));
+  referee.prototype.call <- function(predictions, target.data, score.function) {
+    #browser();
+    latitude <- predictions$latitude.fit;
+    longitude <- predictions$longitude.fit;
+    return(score.function(latitude, longitude, xvalid=target.data$latitude, yvalid=target.data$latitude));
   }
   
-  predictions <- lapply(history.models, prediction.prototype.call, target.data=newdata);
-  histories <- lapply(history.models, referee.prototype.call, target.data=newdata, score.function=score.fn);
+  predictions <- lapply(history.models$trained.models, prediction.prototype.call, target.data=newdata);
+  histories <- lapply(predictions, referee.prototype.call, target.data=newdata, score.function=score.fn);
   
   history.models <- mapply(list,
                            trained.model=models,
@@ -30,19 +34,25 @@ mixture.predict <- function(models, newdata, score.fn, mixture) {
 
   ## input for mixture function will be datacube (length(models), length(response.variables), history.size)
   mixture.prototype.call <- function(index, mixture, history.models) {
+
     create.datacube <- function(index, history.models) {
       take.slice <- function(model, index) {
         return(cbind(model$prediction[index,], model$history[index]));
       };
-      datacube <- list();
-      ## todo lapply...
-      for (i in min(1, index - 5) : index) {
-        c(slice, lapply(history.models, take.slice, i));
+
+      ##todo for clarity rewrite this one 
+      data <- data.frame();
+      for (i in max(1, index - 4) : index) {
+        data <- rbind(data, cbind(ldply(history.models, take.slice, i), ID=i));
       };
+      data$row <- 1:nrow(data) %% length(history.models);          
+      m <- melt(data, id.vars = c("row", "ID"));
+      datacube <- acast(m, row ~ variable ~ ID);
       return(datacube);
     };
+    
     return(mixture(create.datacube(index, history.models)));
   }
-  mixture.results <- lapply(1:nrows(newdata), mixture.prototype.call, mixture, history.models);
-  return(data.frame(mixture.results));
+  mixture.results <- lapply(10:nrow(newdata), mixture.prototype.call, mixture, history.models);
+  return(ldply(mixture.results, identity));
 }
