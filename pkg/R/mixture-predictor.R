98cd8030f7ca154e176d974053f5b2367abb0426
Sami Airaksinen <samiaira@gmail.com>
2013-12-11 22:42:16 +0200

    forgot to disable .expand option.

--- pkg/R/mixture-predictor.R
+++ pkg/R/mixture-predictor.R
@@ -6,23 +6,24 @@ mixture.predict <- function(features, models, newdata, score.fn, mixture, t.dist
   # calculates predictor variable vectors for each data point based on the t-values
   feature.extraction.prototype.call <- function(feature, data, t.dist, t.window.length) {
 
-      ## generates data.frames for each row from [data.starttime + distance + length, data.endtime] so that
-      ## it contains data between [row.time - distance - length, row.time - distance]
+    ## generates data.frames for each row from [data.starttime + distance + length, data.endtime] so that
+    ## it contains data between [row.time - distance - length, row.time - distance]
     slice.data <- function(data, time.dist, t.window.length) {
       sliding.cursors <- getBetween(data, min(data$time) + as.numeric(time.dist + t.window.length), max(data$time) - time.dist)
       return(alply(sliding.cursors, 1, function(row) {
         sliding.window <- getBetween(data, row$time - t.window.length, row$time)
         predicted.row <- getClosestTo(data, row$time + time.dist)
         return(rbind(sliding.window, predicted.row))
-      }))
+      }, .expand=FALSE))
     }
 
     dataslices <- slice.data(data, t.dist, t.window.length); ##list of data.frames, size of t.window.length, moved backwards > t.dist, last row is target time
     return(ldply(dataslices, function(sliding.window) {
       last.row <- sliding.window[nrow(sliding.window),]
-      fitted.feature <- feature$fit(sliding.window[-nrow(sliding.window),]);
+      fitted.feature <- feature$fit(sliding.window[-nrow(sliding.window),])
+      ## todo or optionally with(sliding.window,{lm()}) via environment??
       return(predict(fitted.feature, newdata=last.row))
-    }));
+    }))
   }
 
   # returns prediction vectors in data.frame
@@ -32,13 +33,13 @@ mixture.predict <- function(features, models, newdata, score.fn, mixture, t.dist
     return(cbind(latitude=latitude, longitude=longitude));
   }
 
-  # returns score vector length of prediction row count 
+  # returns score vector length of prediction row count
   referee.prototype.call <- function(predictions, target.data, score.function) {
     latitude <- predictions$latitude.fit;
     longitude <- predictions$longitude.fit;
     return(score.function(latitude, longitude, xvalid=target.data$latitude, yvalid=target.data$longitude, scale=5000)); #todo pass this parameter as input (...?)
   }
-  
+
   history.models <- list(trained.models=models);
 
   feature.extraction <- ldply(features, feature.extraction.prototype.call, data=newdata, t.dist=t.dist, t.window.length=t.window.length);
