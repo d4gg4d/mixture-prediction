2f1f1d04cd380bb491d337d9c20f2a50b68fe2ad
Sami Airaksinen <samiaira@gmail.com>
2014-01-20 23:09:39 +0200

    Adding more sanitation for creating error stats.

--- pkg/R/utils.R
+++ pkg/R/utils.R
@@ -16,13 +16,14 @@ lm.create.models <- function(formulas, data) {
 }
 
 ## TODO documentation
-PredictionError <- function(prediction, valid, score.fn) {
+PredictionError <- function(prediction, valid, score.fn=NULL) {
     valid.predicted <- valid[valid$time %in% prediction$time,]
+    valid.predicted <- valid.predicted[with(valid.predicted, !duplicated(time)),]
     valid.predicted <- valid.predicted[with(valid.predicted, order(time)),]
     stopifnot(valid.predicted$time == prediction$time)
     difference.long <- prediction$longitude - valid.predicted$longitude
     difference.lat <- prediction$latitude - valid.predicted$latitude
-    score.value <- score.fn(prediction$longitude, valid.predicted$longitude, prediction$latitude, valid.predicted$latitude)
+    score.value <- ifelse(!is.null(score.fn),score.fn(prediction$longitude, prediction$latitude, xvalid=valid.predicted$longitude, yvalid=valid.predicted$latitude, scale=5000), NA) ##TODO how to pass funtion properly with parameters
     return(data.frame(
       time=prediction$time,
       longitude=difference.long,
