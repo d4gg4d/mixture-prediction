3c7bc2c09a42ad057aa2325f56d0d6cb6f3e2987
Sami Airaksinen <samiaira@gmail.com>
2014-01-25 14:16:55 +0200

    moved inner functions out of mixture predict

--- pkg/R/mixture-predictor.R
+++ pkg/R/mixture-predictor.R
@@ -23,36 +23,6 @@
 #' 
 mixture.predict <- function(features, trained.models, newdata, score.fn, mixture, t.dist, t.window.length, prediction.interval=360) {
 
-  #' takes model pair and predicts values for each target.data row
-  #'
-  #' @param model.pair a list of models that contain trained model for latitude and longitude
-  #'
-  #' @param target.data extracted feature vectors to which predictions are made against
-  #'
-  #' @return prediction dataframe (rows of latitude,longitude pairs)
-  predict.internal <- function(model.pair, target.data) {
-    ##todo check if there are redundant df castings and namings here
-    latitude <- data.frame(latitude=predict(model.pair$latitude, target.data));
-    longitude <- data.frame(longitude=predict(model.pair$longitude, target.data));
-    return(cbind(latitude=latitude, longitude=longitude));
-  }
-
-  #' takes predicted data.frame and calculates validation value(s) based on scoring function
-  #'
-  #' @param predictions (latitude,longitude) pairs which have been predicted (data.frame format)
-  #'
-  #' @param target.data (latitude,longitude) pairs which represent real values (data.frame format)
-  #'
-  #' @param score.fn given function that takes in predicted vector and expected vector and returns scalar value for it
-  #'
-  #' @return value of score.fn applied to predicted and target.data
-  validate.predictions <- function(predictions, target.data, score.fn) {
-    stopifnot(predictions$time == target.data$time)
-    longitude <- predictions$longitude;
-    latitude <- predictions$latitude;
-    return(score.fn(longitude, latitude, xvalid=target.data$longitude, yvalid=target.data$latitude, scale=5000)); #todo pass this parameter as input (...?)
-  }
-
   extracted.features <- features.extraction(features, newdata, t.dist, t.window.length, interval=prediction.interval)
   validation.data <- predicted.target.data(newdata, t.dist, t.window.length, interval=prediction.interval)
   predictions <- lapply(trained.models, predict.internal, target.data=extracted.features)
@@ -64,4 +34,34 @@ mixture.predict <- function(features, trained.models, newdata, score.fn, mixture
                       SIMPLIFY=FALSE);
   final.output <- ldply(validation.data$time, mixture.internal, mixture, histories, t.dist=t.dist, hist.times=take.times(histories))
   return(final.output[with(final.output, !duplicated(time)),])
+}
+
+#' takes model pair and predicts values for each target.data row
+#'
+#' @param model.pair a list of models that contain trained model for latitude and longitude
+#'
+#' @param target.data extracted feature vectors to which predictions are made against
+#'
+#' @return prediction dataframe (rows of latitude,longitude pairs)
+predict.internal <- function(model.pair, target.data) {
+  ##todo check if there are redundant df castings and namings here
+  latitude <- data.frame(latitude=predict(model.pair$latitude, target.data));
+  longitude <- data.frame(longitude=predict(model.pair$longitude, target.data));
+  return(cbind(latitude=latitude, longitude=longitude));
+}
+
+#' takes predicted data.frame and calculates validation value(s) based on scoring function
+#'
+#' @param predictions (latitude,longitude) pairs which have been predicted (data.frame format)
+#'
+#' @param target.data (latitude,longitude) pairs which represent real values (data.frame format)
+#'
+#' @param score.fn given function that takes in predicted vector and expected vector and returns scalar value for it
+#'
+#' @return value of score.fn applied to predicted and target.data
+validate.predictions <- function(predictions, target.data, score.fn) {
+  stopifnot(predictions$time == target.data$time)
+  longitude <- predictions$longitude;
+  latitude <- predictions$latitude;
+  return(score.fn(longitude, latitude, xvalid=target.data$longitude, yvalid=target.data$latitude, scale=5000)); #todo pass this parameter as input (...?)
 }
