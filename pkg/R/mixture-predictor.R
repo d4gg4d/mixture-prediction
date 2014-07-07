b7883c8b7de1255b937d5b4cf7397130cab7d08b
Sami Airaksinen <samiaira@gmail.com>
2014-07-07 23:21:20 +0300

    removed faulty parameterization of time distance
    - there is no use case for moving history window backward

--- pkg/R/mixture-predictor.R
+++ pkg/R/mixture-predictor.R
@@ -25,10 +25,10 @@
 #'
 #' @return dataframe of predicted values
 #'
-MixturePredict <- function(trained.models, mixture, score.function, feature.data, test.data, t.dist, hist.length) {
+MixturePredict <- function(trained.models, mixture, score.function, feature.data, test.data, hist.length) {
   histories <- PredictionsAndValidations(trained.models, score.function, feature.data, test.data)
   validation.columns <- llply(list(score.function), function(el) { return(name(el)) })
-  final.output <- MixtureSelection(mixture, histories, t.dist, hist.length, validation.columns)
+  final.output <- MixtureSelection(mixture, histories, hist.length, validation.columns)
   stopifnot(!duplicated(final.output$time))
   return(final.output)
 }
@@ -108,13 +108,13 @@ ValidatePredictions <- function(predictions, target.data, score.fn) {
 #' 
 #' @return data.frame of final output for each target data
 #' 
-MixtureSelection <- function(mixture, history, t.dist, hist.length, validation.columns) {
-  targets <- getBetween(history, min(history$time) + t.dist + hist.length, max(history$time))
+MixtureSelection <- function(mixture, history, hist.length, validation.columns) {
+  targets <- getBetween(history, min(history$time) + 1 + hist.length, max(history$time))
   times <- targets[with(targets, !duplicated(time)), ]$time
   values <- ldply(times, function(prediction.time) {
     prediction.candidates <- history[history$time == prediction.time, ]
-    history.window <- subset(HistoryWindow(history, prediction.time - t.dist, hist.length), select=unlist(c(".id", "time", validation.columns)))
-    final.model.id <- select(mixture, history.window, t.dist)
+    history.window <- subset(HistoryWindow(history, prediction.time - 1, hist.length), select=unlist(c(".id", "time", validation.columns)))
+    final.model.id <- select(mixture, history.window)
     return(prediction.candidates[prediction.candidates$.id == final.model.id, ])
   })
   return(values)
